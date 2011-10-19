2cddb64a9d70fbe6e52b75a6ac2f41c87ebd55ac
Fredi <fredisoft@gmail.com>
2011-10-18 22:53:34 -0200

    Game: Added classes to work with data stores
    It's a modified version of TrinityCore's DBC loading

---
+++ game/datastores.cpp
@@ -0,0 +1,282 @@
+/**
+ * Studying C++
+ * Simple Turn Based Battle Game
+ * 
+ * Based on TrinityCore's code
+ * https://github.com/TrinityCore/TrinityCore
+ *
+ * Fredi Machado
+ * http://github.com/fredi
+ */
+
+#include "datastores.h"
+
+DataStorage<SpellEntry> sSpellStore(SpellEntryfmt);
+
+uint32 DataFileCount = 0;
+
+template<class T>
+inline void LoadData(DataStorage<T>& storage, std::string const& dataPath, std::string const& filename)
+{
+    ++DataFileCount;
+    std::string dataFilename = dataPath + filename;
+
+    if (!storage.Load(dataFilename.c_str()))
+    {
+        // sort problematic dbc to (1) non compatible and (2) non-existed
+        if (FILE* f = fopen(dataFilename.c_str(), "rb"))
+        {
+            printf("%s has invalid data.\n", dataFilename.c_str());
+            fclose(f);
+        }
+        else
+            printf("Data store %s doesn't exist.\n", dataFilename.c_str());
+    }
+}
+
+void LoadDataStores(const std::string& dataPath)
+{
+    LoadData(sSpellStore, dataPath, "spells.dat");
+
+    printf("Loaded %d data stores.\n", DataFileCount);
+}
+
+DataFileLoader::DataFileLoader()
+{
+    data = NULL;
+    fieldsOffset = NULL;
+}
+
+bool DataFileLoader::Load(const char* fileName, char const* fmt)
+{
+    uint32 header;
+    if (data)
+    {
+        delete[] data;
+        data = NULL;
+    }
+
+    FILE* f = fopen(fileName, "rb");
+    if (!f)
+        return false;
+
+    if (fread(&header, 4, 1, f) != 1)
+    {
+        fclose(f);
+        return false;
+    }
+
+    if (header != 0x454D4147)                                // 'GAME'
+    {
+        fclose(f);
+        return false;
+    }
+
+    if (fread(&recordCount, 4, 1, f) != 1)                   // Number of records
+    {
+        fclose(f);
+        return false;
+    }
+
+    if (fread(&fieldCount, 4, 1, f) != 1)                    // Number of fields
+    {
+        fclose(f);
+        return false;
+    }
+
+    if (fread(&recordSize, 4, 1, f) != 1)                    // Size of a record
+    {
+        fclose(f);
+        return false;
+    }
+
+    if (fread(&stringSize, 4, 1, f) != 1)                    // String size
+    {
+        fclose(f);
+        return false;
+    }
+
+    fieldsOffset = new uint32[fieldCount];
+    fieldsOffset[0] = 0;
+    for (uint32 i = 1; i < fieldCount; ++i)
+    {
+        fieldsOffset[i] = fieldsOffset[i - 1];
+        fieldsOffset[i] += sizeof(uint32);                   // 4 byte fields (int32/float/strings)
+    }
+
+    data = new unsigned char[recordSize * recordCount + stringSize];
+    stringTable = data + recordSize * recordCount;
+
+    if (fread(data, recordSize * recordCount + stringSize, 1, f) != 1)
+    {
+        fclose(f);
+        return false;
+    }
+
+    fclose(f);
+
+    return true;
+}
+
+DataFileLoader::~DataFileLoader()
+{
+    if (data)
+        delete[] data;
+
+    if (fieldsOffset)
+        delete[] fieldsOffset;
+}
+
+DataFileLoader::Record DataFileLoader::getRecord(size_t id)
+{
+    assert(data);
+    return Record(*this, data + id * recordSize);
+}
+
+uint32 DataFileLoader::GetFormatRecordSize(const char* format, int32* index_pos)
+{
+    uint32 recordsize = 0;
+    int32 i = -1;
+    for (uint32 x = 0; format[x]; ++x)
+    {
+        switch (format[x])
+        {
+            case FT_FLOAT:
+                recordsize += sizeof(float);
+                break;
+            case FT_INT:
+                recordsize += sizeof(uint32);
+                break;
+            case FT_STRING:
+                recordsize += sizeof(char*);
+                break;
+            case FT_IND:
+                i = x;
+                recordsize += sizeof(uint32);
+                break;
+            default:
+                assert(false && "Unknown field format character in fmt");
+                break;
+        }
+    }
+
+    if (index_pos)
+        *index_pos = i;
+
+    return recordsize;
+}
+
+char* DataFileLoader::AutoProduceData(char const* format, uint32& records, char**& indexTable)
+{
+    typedef char* ptr;
+    if (strlen(format) != fieldCount)
+        return NULL;
+
+    //get struct size and index pos
+    int32 i;
+    uint32 recordsize = GetFormatRecordSize(format, &i);
+
+    if (i >= 0)
+    {
+        uint32 maxi = 0;
+        //find max index
+        for (uint32 y = 0; y < recordCount; ++y)
+        {
+            uint32 ind = getRecord(y).getUInt(i);
+            if (ind > maxi)
+                maxi = ind;
+        }
+
+        ++maxi;
+        records = maxi;
+        indexTable = new ptr[maxi];
+        memset(indexTable, 0, maxi * sizeof(ptr));
+    }
+    else
+    {
+        records = recordCount;
+        indexTable = new ptr[recordCount];
+    }
+
+    char* dataTable = new char[(recordCount) * recordsize];
+
+    uint32 offset = 0;
+
+    for (uint32 y = 0; y < recordCount; ++y)
+    {
+        if (i >= 0)
+            indexTable[getRecord(y).getUInt(i)] = &dataTable[offset];
+        else
+            indexTable[y] = &dataTable[offset];
+
+        for (uint32 x=0; x < fieldCount; ++x)
+        {
+            switch (format[x])
+            {
+                case FT_FLOAT:
+                    *((float*)(&dataTable[offset])) = getRecord(y).getFloat(x);
+                    offset += sizeof(float);
+                    break;
+                case FT_IND:
+                case FT_INT:
+                    *((uint32*)(&dataTable[offset])) = getRecord(y).getUInt(x);
+                    offset += sizeof(uint32);
+                    break;
+                case FT_STRING:
+                    *((char**)(&dataTable[offset])) = NULL;   // will replace non-empty or "" strings in AutoProduceStrings
+                    offset += sizeof(char*);
+                    break;
+                default:
+                    assert(false && "Unknown field format character in fmt");
+                    break;
+            }
+        }
+    }
+
+    return dataTable;
+}
+
+char* DataFileLoader::AutoProduceStrings(char const* format, char* dataTable)
+{
+    if (strlen(format) != fieldCount)
+        return NULL;
+
+    char* stringPool = new char[stringSize];
+    memcpy(stringPool, stringTable, stringSize);
+
+    uint32 offset = 0;
+
+    for (uint32 y = 0; y < recordCount; ++y)
+    {
+        for (uint32 x = 0; x < fieldCount; ++x)
+        {
+            switch (format[x])
+            {
+                case FT_FLOAT:
+                    offset += sizeof(float);
+                    break;
+                case FT_IND:
+                case FT_INT:
+                    offset += sizeof(uint32);
+                    break;
+                case FT_STRING:
+                {
+                    // fill only not filled entries
+                    char** slot = (char**)(&dataTable[offset]);
+                    if (!*slot || !**slot)
+                    {
+                        const char * st = getRecord(y).getString(x);
+                        *slot=stringPool+(st-(const char*)stringTable);
+                    }
+                    offset += sizeof(char*);
+                    break;
+                 }
+                 default:
+                     assert(false && "Unknown field format character in fmt");
+                     break;
+            }
+        }
+    }
+
+    return stringPool;
+}
---
+++ game/datastores.h
@@ -0,0 +1,182 @@
+/**
+ * Studying C++
+ * Simple Turn Based Battle Game
+ * 
+ * Based on TrinityCore's code
+ * https://github.com/TrinityCore/TrinityCore
+ *
+ * Fredi Machado
+ * http://github.com/fredi
+ */
+
+#ifndef _DATASTORES_H
+#define _DATASTORES_H
+
+#include "common.h"
+#include <list>
+#include <string>
+#include <cstring>
+#include <cassert>
+
+enum
+{
+    FT_STRING = 's',                                        // char*
+    FT_FLOAT  = 'f',                                        // float
+    FT_INT    = 'i',                                        // uint32
+    FT_IND    = 'n'                                         // the same, but parsed to data
+};
+
+void LoadDataStores(const std::string&);
+
+class DataFileLoader
+{
+    public:
+        DataFileLoader();
+        ~DataFileLoader();
+
+        bool Load(char const* fileName, char const* fmt);
+
+        class Record
+        {
+            public:
+                float getFloat(size_t field) const
+                {
+                    assert(field < file.fieldCount);
+                    float val = *reinterpret_cast<float*>(offset + file.GetOffset(field));
+                    return val;
+                }
+                uint32 getUInt(size_t field) const
+                {
+                    assert(field < file.fieldCount);
+                    uint32 val = *reinterpret_cast<uint32*>(offset + file.GetOffset(field));
+                    return val;
+                }
+
+                char const* getString(size_t field) const
+                {
+                    assert(field < file.fieldCount);
+                    size_t stringOffset = getUInt(field);
+                    assert(stringOffset < file.stringSize);
+                    return reinterpret_cast<char*>(file.stringTable + stringOffset);
+                }
+
+            private:
+                Record(DataFileLoader& file_, unsigned char* offset_): offset(offset_), file(file_) {}
+                unsigned char* offset;
+                DataFileLoader& file;
+
+                friend class DataFileLoader;
+
+        };
+
+        // Get record by id
+        Record getRecord(size_t id);
+        /// Get begin iterator over records
+
+        uint32 GetNumRows() const { return recordCount; }
+        uint32 GetRowSize() const { return recordSize; }
+        uint32 GetCols() const { return fieldCount; }
+        uint32 GetOffset(size_t id) const { return (fieldsOffset != NULL && id < fieldCount) ? fieldsOffset[id] : 0; }
+        bool IsLoaded() const { return data != NULL; }
+        char* AutoProduceData(char const* fmt, uint32& count, char**& indexTable);
+        char* AutoProduceStrings(char const* fmt, char* dataTable);
+        static uint32 GetFormatRecordSize(char const* format, int32* index_pos = NULL);
+    private:
+
+        uint32 recordSize;
+        uint32 recordCount;
+        uint32 fieldCount;
+        uint32 stringSize;
+        uint32* fieldsOffset;
+        unsigned char* data;
+        unsigned char* stringTable;
+};
+
+template<class T>
+class DataStorage
+{
+    typedef std::list<char*> StringPoolList;
+    public:
+        explicit DataStorage(const char *f) :
+            fmt(f), nCount(0), fieldCount(0), dataTable(NULL)
+        {
+            indexTable.asT = NULL;
+        }
+        ~DataStorage() { Clear(); }
+
+        T const* LookupEntry(uint32 id) const
+        {
+            return (id >= nCount) ? NULL : indexTable.asT[id];
+        }
+        uint32  GetNumRows() const { return nCount; }
+        char const* GetFormat() const { return fmt; }
+        uint32 GetFieldCount() const { return fieldCount; }
+
+        bool Load(char const* fn)
+        {
+            DataFileLoader data;
+            // Check if load was sucessful, only then continue
+            if (!data.Load(fn, fmt))
+                return false;
+
+            fieldCount = data.GetCols();
+            dataTable = (T*)data.AutoProduceData(fmt, nCount, indexTable.asChar);
+            stringPoolList.push_back(data.AutoProduceStrings(fmt, (char*)dataTable));
+
+            return indexTable.asT != NULL;
+        }
+
+        bool LoadStringsFrom(char const* fn)
+        {
+            // data must be already loaded using Load
+            if (!indexTable.asT)
+                return false;
+
+            DataFileLoader data;
+            // Check if load was successful, only then continue
+            if (!data.Load(fn, fmt))
+                return false;
+
+            stringPoolList.push_back(data.AutoProduceStrings(fmt, (char*)dataTable));
+
+            return true;
+        }
+
+        void Clear()
+        {
+            if (!indexTable.asT)
+                return;
+
+            delete[] ((char*)indexTable.asT);
+            indexTable.asT = NULL;
+            delete[] ((char*)dataTable);
+            dataTable = NULL;
+
+            while (!stringPoolList.empty())
+            {
+                delete[] stringPoolList.front();
+                stringPoolList.pop_front();
+            }
+            nCount = 0;
+        }
+
+    private:
+        char const* fmt;
+        uint32 nCount;
+        uint32 fieldCount;
+
+        union
+        {
+            T** asT;
+            char** asChar;
+        } indexTable;
+
+        T* dataTable;
+        StringPoolList stringPoolList;
+};
+
+extern DataStorage<SpellEntry> sSpellStore;
+
+char const SpellEntryfmt[] = "niiifffiii";
+
+#endif
